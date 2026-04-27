eadd933b3c8285cbab1a03657e0ed31c16dc7b9f
Abeer Garges <agfayez2018@gmail.com>
2026-04-27 13:27:37 -0700

    Add Deposit working

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,17 +1,79 @@
 package com.pluralsight;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
+    static Scanner scanner = new Scanner(System.in);
+
+    static void main() {
+        homeScreen();
+
+    }
+
+    static void homeScreen(){
+        while(true){
+            System.out.println("Welcome to Accounting Ledger Application");
+            System.out.println("D) Add Deposit");
+            System.out.println("P) Make Payment (Debit)");
+            System.out.println("Ledger");
+            System.out.println("E) Exit");
+
+            String userInput = scanner.nextLine();
+            switch (userInput){
+                case "D":
+                    addDeposit();
+                    break;
+                case "P":
+                    System.out.println("payment still not there");
+                    break;
+                case "L":
+                    System.out.println("Ledger still not there");
+                    break;
+                case "E":
+                    System.out.println("Exit");
+                    return;
+                default:
+                    System.out.println("try again");
+                    break;
+            }
+        }
+
+
+    }
+
+    static void addDeposit(){
+        System.out.println("Enter description for your deposit: ");
+        String description = scanner.nextLine();
+
+        System.out.println("Enter the vendor: ");
+        String vendor = scanner.nextLine();
+
+        System.out.println("Enter amount: ");
+        double amount = scanner.nextDouble();
+
+        Transaction transaction = new Transaction(LocalDate.now(), LocalTime.now(), description, vendor, amount);
+
+        String line = transaction.toCsvFormate();
+
+        try{
+            FileWriter fileWriter = new FileWriter("data/transactions.csv", true);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+            bufferedWriter.write(line);
+            bufferedWriter.newLine();
+
+            bufferedWriter.close();
+        }catch (Exception ex){
+            System.out.println("error happen");
+
         }
     }
+
 }
